3524fe2b0825dafc81d7cea759f5ac89aa913dc4
Abdelrahman Sayed <61325578+xBodda@users.noreply.github.com>
2021-05-06 07:59:47 +0200

    Update GBFS.py

--- In Python/GBFS.py
+++ In Python/GBFS.py
@@ -1,6 +1,5 @@
 from queue import PriorityQueue
 
-# Node of the graph
 class Vertex:
     def __init__(self, n):
         self.name = n
@@ -10,26 +9,20 @@ class Vertex:
     def add_neighbor(self, v):
         if v not in self.neighbors:
             self.neighbors.append(v)
-            #self.neighbors.sort()
-
 
 class Graph:
-    # Dictionary "Python" or Hashmap "Java" of Vertices in the graph =>key = vertex name, value = object of vertex
     vertices = {}
 
-    def add_vertex(self, vertex):
+    def New_Vertex(self, vertex):
         if isinstance(vertex, Vertex) and vertex.name not in self.vertices:
             self.vertices[vertex.name] = vertex
             return True
         else:
             return False
 
-    # Create edge between two vertices that already exists in the graph
-    def add_edge(self, vertex1, vertex2):
+    def New_Edge(self, vertex1, vertex2):
         if vertex1 in self.vertices and vertex2 in self.vertices:
-            # key = vertex name, value = object of vertex
             for key, value in self.vertices.items():
-                # Adding each vertex name neighbour to the other vertex with cost
                 if key == vertex1:
                     value.add_neighbor(vertex2)
                 if key == vertex2:
@@ -38,44 +31,40 @@ class Graph:
         else:
             return False
 
-    # UCS Algorithm
-    def uniform_cost_search(self, h, start, goal):
-        count = 0
-        frontier = PriorityQueue()
-        frontier.put((0, [start]))
+    def UCS(self, h, start, goal):
+        Counter = 0
+        Frontier = PriorityQueue()
+        Frontier.put((0, [start]))
        
-        while frontier:
-            count += 1
-            cost, node = frontier.get()
+        while Frontier:
+            Counter += 1
+            cost, node = Frontier.get()
             current = self.vertices[node[len(node) - 1]]
             if current.visited == False:
                 current.visited = True
                 if current.name == goal:
-                    print("Path found by UCS: " + str(node) + ", Count= " + str(count))
+                    print("Path found by UCS: " + str(node) + ", Count= " + str(Counter))
                     break
                 for vertexName in current.neighbors:
                     if self.vertices[vertexName].visited == False:
                         temp = node[:]
                         temp.append(vertexName)
                         heuristicValue = h[vertexName]
-                        frontier.put((heuristicValue, temp))
+                        Frontier.put((heuristicValue, temp))
                 print("Path till now", node)
 
     def print_graph(self):
         for key in sorted(list(self.vertices.keys())):
             print(key + str(self.vertices[key].neighbors))
 
-
-# Create Graph
 g = Graph()
 
 vertices = ['Arad', 'Zerind', 'Timisoara', 'Sibiu', 'Oradea', 'Lugoj', 'RimnicuVilcea',
             'Mehadia', 'Craiova', 'Pitesti', 'Fagaras', 'Dobreta', 'Bucharest', 'Giurgiu']
 
 for i in vertices:
-    g.add_vertex(Vertex(i))
+    g.New_Vertex(Vertex(i))
 
-# g.print_graph()
 edges = {
     'Arad': {'Zerind', 'Sibiu'},
     'Timisoara': {'Arad', 'Lugoj'},
@@ -93,7 +82,7 @@ print(type(vertices))
 
 for vertex1, value in edges.items():
     for vertex2 in edges[vertex1]:
-        g.add_edge(vertex1, vertex2)
+        g.New_Edge(vertex1, vertex2)
 
 heuristicValue =  {'Arad': 366, 'Zerind': 374,
          'Timisoara':329,
@@ -105,5 +94,4 @@ heuristicValue =  {'Arad': 366, 'Zerind': 374,
          'Dobreta': 242, 'Bucharest': 0,
          'Giurgiu': 77}
 
-# g.print_graph()
-g.uniform_cost_search(heuristicValue, 'Arad', "Bucharest")
+g.UCS(heuristicValue, 'Arad', "Bucharest")
